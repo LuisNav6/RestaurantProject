3820483dfdaf4e0f19e2e9ba1a1f5d95ec1e8b9c
Tony <pp.tony777@gmail.com>
2023-06-07 14:05:44 -0600

    Change context of the footer, and style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'angularfirebase-student-app';
+  title = 'Taco-Mundo';
 
   constructor() {}
 
